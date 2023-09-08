6d5e091159285ed8e7780e87b0ed25baa8dc9b9a
KAYSILVHA <kaysilvha@gmail.com>
2023-09-07 22:20:34 -0300

    adicionando funcoes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 const getFrontService = document.getElementById("service-front")
 const getBackService = document.getElementById("service-back")
-const getBackFrontService = document.getElementById("service-back-front")
-
-
+const getBackFrontService = document.getElementById("back-front")
+const getService = document.getElementById("service")
+const getVoltar = document.getElementById("voltar")
 
 getFrontService.addEventListener('click', ()=>{
   front()
@@ -12,14 +12,58 @@ getBackService.addEventListener('click', ()=>{
   back()
 });
 
+getVoltar.addEventListener('click', ()=>{
+  recarregar()
+});
 
 
 function front(){
-  getBackFrontService.setAttribute('style', 'display: none')
-  console.log("Oi")
+  getBackFrontService.setAttribute('class', 'service-back-front desative')
+  const criarAbaFront = document.createElement("div")
+  criarAbaFront.innerHTML = `<div class = "frontend">
+  <h2>Front-End</h2>
+  <a href="/index.html"><img id="voltar" src="/assets/imagens/voltar.png" alt="Voltar"></a>
+  </div>
+  <div class = "itens-service">
+    <div class="itens">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
+  </div>`
+  service.appendChild(criarAbaFront);
 }
 
 function back(){
   getBackFrontService.setAttribute('class', 'service-back-front desative')
-  console.log("Oi")
+  const criarAbaBack = document.createElement("div")
+  criarAbaBack.innerHTML = `<div class = "backend">
+  <h2>Back-End</h2>
+  <a href="/index.html"><img id="voltar" src="/assets/imagens/voltar.png" alt="Voltar"></a>
+  </div>
+  <div class = "itens-service">
+    <div class="itens">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </div>
+  </div>`
+  service.appendChild(criarAbaBack);
+}
+
+
+function recarregar(){
+  location.reload()
 }
